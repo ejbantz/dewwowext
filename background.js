6468ad5886ce8819009baf9ff0b23a4ce297d4da
EJ Bantz <ejbantz@gmail.com>
2022-02-13 09:39:23 -0600

    have a soap call working.

--- background.js
+++ background.js
@@ -19,10 +19,13 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
     // them via code to have a bit more control.  This is demonstrating
     // injecting both a file and function.
     if (tab.url.includes('.force.com/')) {
+      // dewwowext.js is the big one that has all sorts of reusable things.  
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ['library/dewwowext.js']
       });
+
+      // this is showing how to inject just a function. 
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
         func: addMainMenuToSalesforcePage,
@@ -50,39 +53,103 @@ function addMainMenuToSalesforcePage(menuId) {
     // check... but I suppose if someone creates a second extension that happens
     // to add ths same div tag id then bad things would happen.   Maybe the 
     // the div id should be more of random thing... or make use the exensions unique id.
-    dewwowMenu = document.createElement('li');
+    dewwowMenu = document.createElement('div');
     dewwowMenu.id = menuId;
-    dewwowMenu.innerText = 'Dewwow Menu';
     dewwowMenu.className = 'dewwowext-menu slds-global-actions__item slds-dropdown-trigger slds-dropdown-trigger--click';
     
+
+    var content = document.createElement('div');
+    content.innerHTML = (`
+    <div id="myModal" class="dewwowext-modal">
+    <div id="dewwowext-button-test" class="dewwowext-button">Test Button</div>
+    <div id="dewwowext-button-get-objects" class="dewwowext-button">Get Objects</div>
+    <div class="dewwowext-modal-content"></div>
+      <div class="dewwowext-modal-close-button">[X]</div>
+    </div>`);
+    dewwowMenu.appendChild(content);
+
     // This adds an item to the global actions in the upper right corner.
     var parent = document.getElementsByClassName('slds-global-actions')[0];
     if (parent) {
       parent.appendChild(dewwowMenu);
-    
+      parent.appendChild(content);
+
       // Add a handler so we can do things when the user clicks the UI we added. 
       dewwowMenu.addEventListener('click', function(e) {
-        // $DewwowExt is available because it as also injected. 
-        $DewwowExt.log('Menu button clicked.');
+        var modal = document.querySelector('.dewwowext-modal');
+        modal.classList.toggle('dewwowext-modal-open');
+        // $DewwowExt is available because it was also injected.  Just keep in mind
+        // the instance of $DewwowExt is a different instance that is alive within the 
+        // running background.js.  
+      });
+    }
+
+
+    // Demonstrating getting data via test.  Not really doing anything productive with the data yet.
+    var getObjectsButton = document.querySelector('#dewwowext-button-get-objects');
+    if (getObjectsButton) {
+      getObjectsButton.addEventListener('click', function(e) {
+        var modalcontent = document.querySelector('.dewwowext-modal-content');
+        modalcontent.innerText = 'Getting objects...';
         chrome.runtime.sendMessage({action: $DewwowExt.MESSAGES.GET_SESSION}, function(session) {
           $DewwowExt.getSobjects(session.domainAPI, session.sid, function(data){
             // This is a describe of all the objects in the org.
-            $DewwowExt.log(  JSON.stringify(data));  
-          })
+            var modalcontent = document.querySelector('.dewwowext-modal-content');
+            modalcontent.innerText = JSON.stringify(data);
+          });
         });
       });
     }
+
+    // putting stuff in here that is in flight.
+    var testButton = document.querySelector('#dewwowext-button-test');
+    if (testButton) {
+      testButton.addEventListener('click', function(e) {
+        var modalcontent = document.querySelector('.dewwowext-modal-content');
+        modalcontent.innerText = 'Starting Retrieve...';
+        chrome.runtime.sendMessage({action: $DewwowExt.MESSAGES.GET_SESSION}, function(session) {
+           chrome.runtime.sendMessage (
+             {action: $DewwowExt.MESSAGES.METADATA_RETRIEVE,
+              session: session}, 
+              function(data) {
+                console.log('METADATA_RETRIEVE finished.');
+                var modalcontent = document.querySelector('.dewwowext-modal-content');
+                modalcontent.innerText = JSON.stringify(data);
+              }
+           );
+        });
+      });
+    }
+
+    var closeButton = document.querySelector('.dewwowext-modal-close-button');
+    closeButton.addEventListener('click', function(e){
+      var modal = document.querySelector('.dewwowext-modal');
+      modal.classList.toggle('dewwowext-modal-open');
+    })
+
   }
 }
 
 
-// Messages from from the Salesforce page and are received here.   This 
-// message processor fetches data and then passes back to the Salesforce page
-// for it to be displayed. 
+// this onMessage handler is an example of how to pass info from the Salsforce page
+// into the background process.   Some and use sendMessage and it is received here.
+// in most cases you probably don't need to use this but it's handy to know how to do it.
 chrome.runtime.onMessage.addListener(function (message, sender, callback) {
   if(message.action === window.$DewwowExt.MESSAGES.GET_SESSION){
       // The org id is in a cookie called sid.  
       // We get that cookie for the tab url we are on.
+
+      // This is what the sesssion object looks like that is passed back. 
+      /*
+      domain: "ejb-youtube.my.salesforce.com"
+      domainAPI: "ejb-youtube.my.salesforce.com"
+      isActive: false
+      isMaster: true
+      oid: "-- ORG ID ---"
+      server: "ejb-youtube"
+      sid: "----SESSION ID------"
+      */
+
     chrome.cookies.getAll(
       {
         name: "sid",
@@ -101,8 +168,19 @@ chrome.runtime.onMessage.addListener(function (message, sender, callback) {
       }
     );
     return true; // async return
+
+  } else if(message.action === window.$DewwowExt.MESSAGES.METADATA_RETRIEVE) {
+     window.$DewwowExt.startMetadataRetrieve(message.session.domainAPI, message.session.oid, message.session.sid, function(data){
+        console.log(data);
+        callback(data);
+     });
+    return true; // async return
+
   } else {
     return false; // non-async return
+
   } 
+
+  
     
 });
